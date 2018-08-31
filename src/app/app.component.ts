b7173ba47f19df82acff20a719ceb896c931dd7e
JL <dreambeast@yahoo.com>
2018-08-31 13:20:47 +0800

    first checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LPItem } from './components/lp-enter.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lp-collection-app';
+
+  LPList: LPItem [] = [ { //test data
+    name: "A Love Supreme",
+    artist: "John Coltrane",
+    rating: 9,
+    image: "assets/a_love_supreme.jpg"
+  },
+  { 
+    name: "Kind Of Blue",
+    artist: "Miles Davis",
+    rating: 10,
+    image: "assets/kind_of_blue.jpg"
+  }
+];
+
+  addToList(eventObject: LPItem) {
+    console.log('added: ', eventObject);
+    this.LPList.unshift(eventObject);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LpEnterComponent } from './components/lp-enter.component';
+import { LpListComponent } from './components/lp-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LpEnterComponent,
+    LpListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
