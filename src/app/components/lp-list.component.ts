bb487c6f5cfcea21b5ed6d025871583b9676cb13
JL <dreambeast@yahoo.com>
2018-08-31 15:00:25 +0800

    added share button

--- src/app/components/lp-list.component.ts
+++ src/app/components/lp-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LPItem } from './lp-enter.component';
+import { NgNavigatorShareService } from 'ng-navigator-share';
 
 @Component({
   selector: 'app-lp-list',
@@ -9,10 +10,28 @@ import { LPItem } from './lp-enter.component';
 export class LpListComponent implements OnInit {
   @Input() //attributes
   myList: LPItem[] = []; //default empty list
-  
-  constructor() { }
+  private ngNavigatorShareService: NgNavigatorShareService;
+  constructor(
+    ngNavigatorShareService: NgNavigatorShareService
+  ) {
+    this.ngNavigatorShareService = ngNavigatorShareService;
+  }
 
   ngOnInit() {
   }
 
+  share(index: number) {
+    console.log('share');
+    console.log(this.myList[index]);
+    this.ngNavigatorShareService.share({
+      title: this.myList[index].name,
+      text: this.myList[index].artist,
+      url: 'https://developers.google.com/web'
+    }).then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
 }
